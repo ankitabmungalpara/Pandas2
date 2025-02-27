e8edb53eb3f05e91415ffb6abb34ebafdd071a3f
ankitabmungalpara <mungalpara.ankita@gmail.com>
2025-02-27 16:50:15 -0500

    Create invalid-tweets.py

---
+++ invalid-tweets.py
@@ -0,0 +1,56 @@
+"""
+
+Table: Tweets
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| tweet_id       | int     |
+| content        | varchar |
++----------------+---------+
+tweet_id is the primary key (column with unique values) for this table.
+content consists of characters on an American Keyboard, and no other special characters.
+This table contains all the tweets in a social media app.
+ 
+
+Write a solution to find the IDs of the invalid tweets. The tweet is invalid if the number of characters used in the content of the tweet is strictly greater than 15.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+
+Example 1:
+
+Input: 
+Tweets table:
++----------+-----------------------------------+
+| tweet_id | content                           |
++----------+-----------------------------------+
+| 1        | Let us Code                       |
+| 2        | More than fifteen chars are here! |
++----------+-----------------------------------+
+
+Output: 
++----------+
+| tweet_id |
++----------+
+| 2        |
++----------+
+
+Explanation: 
+Tweet 1 has length = 11. It is a valid tweet.
+Tweet 2 has length = 33. It is an invalid tweet.
+
+"""
+
+# The function filters out tweets where the 'content' length is 15 characters or less.  
+# It creates a boolean mask to identify valid tweets and selects only those that meet the condition.  
+# Finally, it returns a DataFrame containing only the 'tweet_id' column of the valid tweets.  
+
+import pandas as pd
+
+def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
+   isValid = tweets['content'].str.len() > 15
+   df = tweets[isValid]
+   return df[['tweet_id']]
